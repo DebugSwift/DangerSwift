5ec8c27157bf3e51ef8315dd2ad349059ff12708
Matheus Gois <matheus.gois@btgpactual.com>
2024-06-06 19:15:26 -0300

    [Main] - Remove Example 🔇

--- Example/script-example.sh
+++
@@ -1,6 +0,0 @@
-# git clone https://github.com/MaatheusGois/DangerSwift && rm -rf DangerSwift/.git
-swift run DangerSwift/danger-swift ci
-brew install danger/tap/danger-js
-swift build
-swift run danger-swift ci --verbose
-echo "Danger Lint stage complete."
